647c708189ff1ff32b903273c797ac4638be9062
NoahAncolie <noahancolie@gmail.com>
2022-03-22 16:35:13 +0100

    sv

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'principal/index'
+  root 'principal#index'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :firms do
+    resources :comments
+  end
 end
